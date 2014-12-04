cef1fa9745c15dd9f3bd241beac2b263e9976ead
Nicholas Holley <nick@zcid.co>
2014-12-04 15:23:41 -0700

    Add caching to store page

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,8 @@ class Product < ActiveRecord::Base
     message: 'must be a URL for GIF, JPG, or PNG image.'
   }
   validates :title, length: {minimum: 10}
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
